cadf4d65c295ae7927975a8fd603eb8e6d2a27d1
Ian Heggaton <ian@pixelated.com.au>
2024-11-01 13:24:55 +1100

    Add StreamlineServiceProvider.php for service registration and configuration

---
+++ src/StreamlineServiceProvider.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace Pixelated\Streamline;
+
+use Composer\InstalledVersions;
+use Illuminate\Foundation\Application;
+use Illuminate\Foundation\Console\AboutCommand;
+use Illuminate\Support\Str;
+use Pixelated\Streamline\Commands\InstallCommand;
+use Pixelated\Streamline\Commands\ListCommand;
+use Pixelated\Streamline\Commands\UpdateCommand;
+use Spatie\LaravelPackageTools\Package;
+use Spatie\LaravelPackageTools\PackageServiceProvider;
+
+class StreamlineServiceProvider extends PackageServiceProvider
+{
+    public function configurePackage(Package $package): void
+    {
+        /*
+         * This class is a Package Service Provider
+         *
+         * More info: https://github.com/spatie/laravel-package-tools
+         */
+        $package
+            ->name('streamline')
+            ->hasConfigFile()
+            ->hasViews()
+            ->hasMigration('create_streamline_table')
+            ->hasCommands(InstallCommand::class, ListCommand::class, UpdateCommand::class);
+    }
+
+    public function boot(): void
+    {
+        $this->registerAppUpdater();
+        AboutCommand::add('Streamline Updater', ['<fg=bright-magenta>Version</>' => $this->getVersionInfo()]);
+    }
+
+    protected function getVersionInfo(): string
+    {
+        return '<fg=bright-magenta>' .
+            InstalledVersions::getPrettyVersion("pixelated-au/streamline") .
+            '</>';
+    }
+
+    protected function registerAppUpdater(): void
+    {
+        $this->app->singleton(AppUpdater::class, fn(Application $app) => new AppUpdater(
+            $app['files'],
+            $app->basePath('stubs'),
+            static fn() => base64_encode(password_hash(Str::random(20), PASSWORD_BCRYPT))
+        ));
+    }
+
+}
