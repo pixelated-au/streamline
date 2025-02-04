c976acb024dea741cdff88f50efe4d41563a1a78
Ian Heggaton <ian@pixelated.com.au>
2025-02-04 11:51:03 +1100

    Integrate OutputSubProcessCalls trait into CheckCommand class

--- src/Commands/CheckCommand.php
+++ src/Commands/CheckCommand.php
@@ -4,10 +4,12 @@ namespace Pixelated\Streamline\Commands;
 
 use Illuminate\Console\Command;
 use Pixelated\Streamline\Actions\CheckAvailableVersions;
+use Pixelated\Streamline\Commands\Traits\OutputSubProcessCalls;
 use RuntimeException;
 
 class CheckCommand extends Command
 {
+    use OutputSubProcessCalls;
 
     public $signature = 'streamline:check
     {--force : Retrieve the most recent versions from GitHub}';
@@ -22,6 +24,8 @@ class CheckCommand extends Command
 
     public function handle(): int
     {
+        $this->listenForSubProcessEvents();
+
         try {
             $nextVersion = $this->availableVersions->execute($this->option('force'));
             $this->info('Next available version: ' . $nextVersion);
