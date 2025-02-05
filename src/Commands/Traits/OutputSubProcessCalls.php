e39b75269f3fa08fb6e3e1e07519fc5c4440efd1
Ian Heggaton <ian@pixelated.com.au>
2025-02-05 11:54:30 +1100

    Add verbosity check for event actions in OutputSubProcessCalls
    - Replace anonymous function with a new function that includes verbosity check before executing event actions

--- src/Commands/Traits/OutputSubProcessCalls.php
+++ src/Commands/Traits/OutputSubProcessCalls.php
@@ -6,21 +6,25 @@ use Illuminate\Support\Facades\Event;
 use Pixelated\Streamline\Events\CommandClassCallback;
 
 trait OutputSubProcessCalls {
-    /**
-     * @noinspection PhpVoidFunctionResultUsedInspection
-     */
+
     private function listenForSubProcessEvents(): void
     {
         // @codeCoverageIgnoreStart
         Event::listen(
             CommandClassCallback::class,
-            fn(CommandClassCallback $event) => match ($event->action) {
-                'comment' => $this->comment($event->value),
-                'info' => $this->info($event->value),
-                'warn' => $this->warn($event->value),
-                'error' => $this->error($event->value),
-                'call' => $this->call($event->value),
-                default => null,
+            function (CommandClassCallback $event) {
+                $verbosity = $this->output->getVerbosity();
+                if ($verbosity < $event->verbosity) {
+                    return;
+                }
+                match ($event->action) {
+                    'comment' => $this->comment($event->value),
+                    'info' => $this->info($event->value),
+                    'warn' => $this->warn($event->value),
+                    'error' => $this->error($event->value),
+                    'call' => $this->call($event->value),
+                    default => null,
+                };
             }
         );
         // @codeCoverageIgnoreEnd
