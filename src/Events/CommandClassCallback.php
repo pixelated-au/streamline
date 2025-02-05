7d772bb2873292c3af77a4bdd6369544f65160d5
Ian Heggaton <ian@pixelated.com.au>
2025-02-05 11:53:58 +1100

    Add verbosity property to CommandClassCallback
    - Added `use Symfony\Component\Console\Output\OutputInterface;` for verbosity levels.
    - Introduced `public int $verbosity` property with a default value of `OutputInterface::VERBOSITY_NORMAL`.

--- src/Events/CommandClassCallback.php
+++ src/Events/CommandClassCallback.php
@@ -2,11 +2,15 @@
 
 namespace Pixelated\Streamline\Events;
 
+use Symfony\Component\Console\Output\OutputInterface;
+
 readonly class CommandClassCallback
 {
     public function __construct(
         public string $action,
-        public string $value
-    ) {
+        public string $value,
+        public int    $verbosity = OutputInterface::VERBOSITY_NORMAL,
+    )
+    {
     }
 }
