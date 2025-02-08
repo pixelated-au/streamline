8d07076b0476a1abda5d3dd373196cfa94edd70b
Ian Heggaton <ian@pixelated.com.au>
2025-02-09 07:19:07 +1100

    Remove unused functions from Helpers.php

--- tests/Helpers.php
+++ tests/Helpers.php
@@ -45,39 +45,6 @@ function working_path(string $path = ''): string
     return laravel_path($path);
 }
 
-function mock_public_path(string $path = ''): string
-{
-    $path = $path ? "public/$path" : 'public';
-    return working_path($path);
-}
-
-function createTestableZipFile(string $zipFileWorkingDir, string $newReleaseArchiveFileName, ?array $files = null): ZipArchive
-{
-    $zip = new ZipArchive();
-    $zip->open(working_path($newReleaseArchiveFileName), ZipArchive::CREATE);
-
-    collect($files ?? [
-        'app/test.php',
-        'public/build/file1.txt',
-        'public/build/file2.txt',
-        'public/build/dir1/file3.txt',
-        'public/build/dir1/dir2/file4.txt',
-        'public/build/dir1/bad.foo',
-        'public/build/file5.txt',
-        'public/build/bad.foo',
-    ])
-        ->each(function (string $file) use ($zip) {
-//            File::makeDirectory(dirname("$zipFileWorkingDir/$file"), recursive: true, force: true);
-//            File::put(path:"$zipFileWorkingDir/$file", contents: "This file has the name: $file");
-            $zip->addFromString($file, "This file has the name: $file");
-        })
-        ->tap(fn() => $zip->close())
-//        ->each(fn(string $file) => unlink("$zipFileWorkingDir/$file"))
-        ->tap(fn() => deleteDirectory($zipFileWorkingDir));
-
-    return $zip;
-}
-
 function deleteDirectory(string $directory): bool
 {
     // Normalize the directory path and ensure it exists
