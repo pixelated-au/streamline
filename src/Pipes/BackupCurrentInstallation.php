21935c38747b873f3f23926a7692acfbbbe660c0
Ian Heggaton <ian@pixelated.com.au>
2025-02-11 13:53:48 +1100

    Moved functionality into CreateArchive class

--- src/Pipes/BackupCurrentInstallation.php
+++ src/Pipes/BackupCurrentInstallation.php
@@ -2,33 +2,22 @@
 
 namespace Pixelated\Streamline\Pipes;
 
-use Illuminate\Support\Facades\Event;
 use Pixelated\Streamline\Actions\CreateArchive;
-use Pixelated\Streamline\Events\CommandClassCallback;
 use Pixelated\Streamline\Interfaces\UpdateBuilderInterface;
 use Pixelated\Streamline\Pipeline\Pipe;
 
-class BackupCurrentInstallation implements Pipe
+readonly class BackupCurrentInstallation implements Pipe
 {
+    public function __construct(private CreateArchive $createArchive)
+    {
+    }
+
     /**
      * @throws \Illuminate\Contracts\Container\BindingResolutionException
      */
     public function __invoke(UpdateBuilderInterface $builder): UpdateBuilderInterface
     {
-        $sourceDir = base_path();
-        $backupDir = config('streamline.backup_dir');
-        $filename  = 'backup-' . date('Ymd_His') . '.tgz';
-
-        Event::dispatch(new CommandClassCallback('info', "Backing up the current installation to $backupDir/$filename"));
-
-        $createArchive = app()->make(CreateArchive::class, [
-            'sourceFolder'    => $sourceDir,
-            'destinationPath' => $backupDir,
-            'filename'        => $filename,
-        ]);
-
-        $createArchive->create();
-
+        $this->createArchive->create();
         return $builder;
     }
 }
