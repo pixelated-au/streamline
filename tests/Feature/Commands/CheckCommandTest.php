925ad416bcf970fcc9007434f1249ff7189081a4
Ian Heggaton <ian@pixelated.com.au>
2025-02-21 09:57:12 +1100

    Remove unused Artisan mock instances and streamline setDefaults method in CheckCommandTest

--- tests/Feature/Commands/CheckCommandTest.php
+++ tests/Feature/Commands/CheckCommandTest.php
@@ -1,7 +1,5 @@
 <?php
 
-use Illuminate\Support\Facades\Artisan;
-use Mockery\MockInterface;
 use Pixelated\Streamline\Tests\Feature\Traits\CheckCommandCommon;
 use Pixelated\Streamline\Tests\Feature\Traits\HttpMock;
 
@@ -22,13 +20,6 @@ it('checks for available updates with a remote request', function () {
         ->mockCache();
     $this->mockHttpReleases();
 
-    $this->instance(Artisan::class, fn (MockInterface $mock) => $mock
-        ->shouldReceive('call')
-        ->once()
-        ->with('streamline:list')
-        ->andReturn(0)
-    );
-
     $this->artisan('streamline:check')
         ->expectsOutputToContain('Next available version: v2.8.6')
         ->assertExitCode(0);
@@ -39,32 +30,15 @@ it('checks for available updates forcing a remote request', function () {
         ->mockCache();
     $this->mockHttpReleases();
 
-    $this->instance(Artisan::class, fn (MockInterface $mock) => $mock
-        ->shouldReceive('call')
-        ->once()
-        ->with('streamline:list')
-        ->andReturn(0)
-    );
-
     $this->artisan('streamline:check --force')
         ->expectsOutputToContain('Next available version: v2.8.6')
         ->assertExitCode(0);
 });
 
 it('throws an exception when doing a remote request because a version is missing', function () {
-    $this->setDefaults([
-        'nextAvailableVersion' => null,
-        'availableVersions' => null,
-    ])
+    $this->setDefaults(['nextAvailableVersion' => null, 'availableVersions' => null])
         ->mockCache();
     $this->mockHttpReleases();
-
-    $this->instance(Artisan::class, fn (MockInterface $mock) => $mock
-        ->shouldReceive('call')
-        ->once()
-        ->with('streamline:list')
-        ->andReturn(0)
-    );
 
     $this->artisan('streamline:check')
         ->expectsOutputToContain('The query to the GitHub repository appeared successful but no versions have been stored')
