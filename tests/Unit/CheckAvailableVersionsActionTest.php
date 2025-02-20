7692df6a978d08942b9a9c625207a201f7998ae9
Ian Heggaton <ian@pixelated.com.au>
2025-02-21 09:57:06 +1100

    Add tests for pre-release and force options in CheckAvailableVersionsActionTest
    - Add test to verify the latest pre-release version is returned when ignorePreReleases is false
    - Add test to ensure all versions are refreshed when a next version exists and force is true
    - Update Event::assertDispatchedTimes assertion method usage

--- tests/Unit/CheckAvailableVersionsActionTest.php
+++ tests/Unit/CheckAvailableVersionsActionTest.php
@@ -18,11 +18,58 @@ it('should return the latest non-prerelease version', function () {
     $cacheMock->shouldReceive('forever')
         ->andReturnTrue();
 
+    Artisan::shouldReceive('call')->with('streamline:list')->andReturn(0);
     Event::fake();
 
     $checkAvailableVersions = new CheckAvailableVersions;
 
     $result = $checkAvailableVersions->execute();
     expect($result)->toBe($versions[2]);
-    Event::assertDispatchedTimes(CommandClassCallback::class, 2);
+    Event::assertDispatchedTimes(CommandClassCallback::class);
+});
+
+it('should return the latest pre-release version when ignorePreReleases is true', function () {
+    $versions = ['v2.1.6b', 'v2.1.5-beta', 'v2.1.4', 'v1.0.0'];
+
+    $cacheMock = Cache::partialMock();
+    $cacheMock->shouldReceive('get')
+        ->with(CacheKeysEnum::NEXT_AVAILABLE_VERSION->value)
+        ->andReturnNull();
+    $cacheMock->shouldReceive('get')
+        ->with(CacheKeysEnum::AVAILABLE_VERSIONS->value)
+        ->andReturn($versions);
+    $cacheMock->shouldReceive('forever')
+        ->andReturnTrue();
+
+    Artisan::shouldReceive('call')->with('streamline:list')->andReturn(0);
+    Event::fake();
+
+    $checkAvailableVersions = new CheckAvailableVersions;
+
+    $result = $checkAvailableVersions->execute(ignorePreReleases: false);
+    expect($result)->toBe($versions[0]);
+    Event::assertDispatchedTimes(CommandClassCallback::class);
+});
+
+it('should refresh all available versions when there is an existing "next version" when force is used', function () {
+    $versions = ['v2.1.6b', 'v2.1.5', 'v2.1.4', 'v1.0.0'];
+
+    $cacheMock = Cache::partialMock();
+    $cacheMock->shouldReceive('get')
+        ->with(CacheKeysEnum::NEXT_AVAILABLE_VERSION->value)
+        ->andReturn($versions[3]);
+    $cacheMock->shouldReceive('get')
+        ->with(CacheKeysEnum::AVAILABLE_VERSIONS->value)
+        ->andReturn($versions);
+    $cacheMock->shouldReceive('forever')
+        ->andReturnTrue();
+
+    Artisan::shouldReceive('call')->with('streamline:list')->andReturn(0);
+    Event::fake();
+
+    $checkAvailableVersions = new CheckAvailableVersions;
+
+    $result = $checkAvailableVersions->execute(force: true);
+    expect($result)->toBe($versions[1]);
+    Event::assertDispatchedTimes(CommandClassCallback::class);
 });
