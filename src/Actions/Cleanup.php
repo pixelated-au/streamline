df6e042d14b328200cca30243564edbd1c75542d
Ian Heggaton <ian@pixelated.com.au>
2025-02-26 11:52:40 +1100

    Refactor Cleanup class to improve functionality
    - Modify methods for enhanced performance
    - Reorganize logic to simplify code structure
    - Remove deprecated functions and replace with updated alternatives

---
+++ src/Actions/Cleanup.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Pixelated\Streamline\Actions;
+
+use Illuminate\Support\Facades\File;
+use Pixelated\Streamline\Events\CommandClassCallback;
+
+class Cleanup
+{
+    public function __invoke(string $tempDir): void
+    {
+        CommandClassCallback::dispatch('info', "Purging the temporary work directory: $tempDir");
+        if (File::deleteDirectory($tempDir)) {
+            CommandClassCallback::dispatch('success', "Temporary work directory purged successfully: $tempDir");
+        }
+    }
+}
