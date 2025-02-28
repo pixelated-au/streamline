e55c105d916b6a167c5bd27dba6e787ce121a05a
Ian Heggaton <ian@pixelated.com.au>
2025-03-01 07:38:00 +1100

    Add version configuration retrieval in UpdateCommand class

--- src/Commands/UpdateCommand.php
+++ src/Commands/UpdateCommand.php
@@ -3,6 +3,7 @@
 namespace Pixelated\Streamline\Commands;
 
 use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Config;
 use Pixelated\Streamline\Commands\Traits\GitHubApi;
 use Pixelated\Streamline\Commands\Traits\OutputSubProcessCalls;
 use Pixelated\Streamline\Pipeline\Pipeline;
@@ -29,6 +30,7 @@ class UpdateCommand extends Command
 
         $builder = (new UpdateBuilder)
             ->setRequestedVersion($this->option('install-version'))
+            ->setCurrentlyInstalledVersion(Config::get('streamline.installed_version'))
             ->forceUpdate($this->option('force'));
 
         return (new Pipeline($builder))
