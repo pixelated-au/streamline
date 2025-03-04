ce7a3312537ff840953169f23311756cd6daebb6
Ian Heggaton <ian@pixelated.com.au>
2025-03-05 10:46:03 +1100

    Add FinishUpdateCommand class for handling update completion tasks

---
+++ src/Commands/FinishUpdateCommand.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Pixelated\Streamline\Commands;
+
+use Illuminate\Console\Command;
+use Illuminate\Support\Env;
+use Illuminate\Support\Facades\Cache;
+use Illuminate\Support\Facades\Config;
+use Pixelated\Streamline\Actions\Cleanup;
+use Pixelated\Streamline\Commands\Traits\OutputSubProcessCalls;
+use Pixelated\Streamline\Enums\CacheKeysEnum;
+use Pixelated\Streamline\Events\InstalledVersionSet;
+
+class FinishUpdateCommand extends Command
+{
+    use OutputSubProcessCalls;
+
+    protected $signature = 'streamline:finish-update';
+    protected $description = 'Run this only after the update is complete. It really should only be run as part of the update pipeline.';
+    protected $hidden = true;
+    protected $isolated = true;
+
+    public function handle(): void
+    {
+        resolve(Cleanup::class)(Config::get('streamline.work_temp_dir'));
+
+        // $installedVersion may be falsy if there was a failure
+        if ($installedVersion = Env::get('STREAMLINE_APPLICATION_VERSION_INSTALLED')) {
+            $this->info("Persisting the new version number ($installedVersion) to the cache.");
+            Cache::put(CacheKeysEnum::INSTALLED_VERSION->value, $installedVersion);
+            InstalledVersionSet::dispatch($installedVersion);
+        }
+    }
+}
