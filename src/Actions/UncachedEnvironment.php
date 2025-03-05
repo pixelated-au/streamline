1ad86667fc16b59281759a0709a026e1e291aa24
Ian Heggaton <ian@pixelated.com.au>
2025-03-06 08:32:04 +1100

    Add UncachedEnvironment.php to manage non-cached configurations

---
+++ src/Actions/UncachedEnvironment.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Pixelated\Streamline\Actions;
+
+use Dotenv\Dotenv;
+
+/** @codeCoverageIgnore */
+class UncachedEnvironment
+{
+    public function get($key, $default = null): string
+    {
+        $currentEnv = Dotenv::createArrayBacked(base_path())->load();
+
+        return isset($currentEnv[$key]) ?: $default;
+    }
+}
