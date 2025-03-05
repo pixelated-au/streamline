65dc56148553a190656505a9e2167c4a7b2057aa
Ian Heggaton <ian@pixelated.com.au>
2025-03-05 16:38:10 +1100

    Replace environment variable access with configuration call in FinishUpdateCommand class

--- src/Commands/FinishUpdateCommand.php
+++ src/Commands/FinishUpdateCommand.php
@@ -3,7 +3,6 @@
 namespace Pixelated\Streamline\Commands;
 
 use Illuminate\Console\Command;
-use Illuminate\Support\Env;
 use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\Config;
 use Pixelated\Streamline\Actions\Cleanup;
@@ -27,8 +26,10 @@ class FinishUpdateCommand extends Command
     {
         resolve(Cleanup::class)(Config::get('streamline.work_temp_dir'));
 
+        $this->call('config:clear');
+
         // $installedVersion may be falsy if there was a failure
-        if ($installedVersion = Env::get('STREAMLINE_APPLICATION_VERSION_INSTALLED')) {
+        if ($installedVersion = Config::get('streamline.installed_version')) {
             $this->info("Persisting the new version number ($installedVersion) to the cache.");
             Cache::put(CacheKeysEnum::INSTALLED_VERSION->value, $installedVersion);
             InstalledVersionSet::dispatch($installedVersion);
