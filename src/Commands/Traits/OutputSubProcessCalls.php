8ab87a3f6b29c273ab63c5c2dc4c86374d6c1f64
Ian Heggaton <ian@pixelated.com.au>
2025-03-09 07:59:07 +1100

    Add logging to OutputSubProcessCalls class methods and refactor event output handling
    - Implement logging in comment, info, warn, and error methods
    - Introduce outputEvent method to encapsulate event action handling logic

--- src/Commands/Traits/OutputSubProcessCalls.php
+++ src/Commands/Traits/OutputSubProcessCalls.php
@@ -1,15 +1,43 @@
 <?php
 
+/** @noinspection PhpMultipleClassDeclarationsInspection */
+
 namespace Pixelated\Streamline\Commands\Traits;
 
+use Illuminate\Support\Facades\App;
 use Illuminate\Support\Facades\Event;
+use Illuminate\Support\Facades\Log;
 use Pixelated\Streamline\Events\CommandClassCallback;
 
+/** @codeCoverageIgnore * */
 trait OutputSubProcessCalls
 {
+    public function comment($message, $verbosity = null): void
+    {
+        !App::runningUnitTests() && Log::debug($message);
+        parent::comment($message, $verbosity);
+    }
+
+    public function info($message, $verbosity = null): void
+    {
+        !App::runningUnitTests() && Log::info($message);
+        parent::info($message, $verbosity);
+    }
+
+    public function warn($message, $verbosity = null): void
+    {
+        !App::runningUnitTests() && Log::warning($message);
+        parent::warn($message, $verbosity);
+    }
+
+    public function error($message, $verbosity = null): void
+    {
+        !App::runningUnitTests() && Log::error($message);
+        parent::error($message, $verbosity);
+    }
+
     private function listenForSubProcessEvents(): void
     {
-        // @codeCoverageIgnoreStart
         Event::listen(
             CommandClassCallback::class,
             function(CommandClassCallback $event) {
@@ -18,16 +46,20 @@ trait OutputSubProcessCalls
                 if ($verbosity < $event->verbosity) {
                     return;
                 }
-                match ($event->action) {
-                    'comment' => $this->comment($event->value),
-                    'info'    => $this->info($event->value),
-                    'warn'    => $this->warn($event->value),
-                    'error'   => $this->error($event->value),
-                    'call'    => $this->call($event->value),
-                    default   => $this->line($event->value),
-                };
+                $this->outputEvent($event);
             }
         );
-        // @codeCoverageIgnoreEnd
+    }
+
+    public function outputEvent(CommandClassCallback $event): void
+    {
+        match ($event->action) {
+            'comment' => $this->comment($event->value),
+            'info'    => $this->info($event->value),
+            'warn'    => $this->warn($event->value),
+            'error'   => $this->error($event->value),
+            'call'    => $this->call($event->value),
+            default   => $this->line($event->value),
+        };
     }
 }
