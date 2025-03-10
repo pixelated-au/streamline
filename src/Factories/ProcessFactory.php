2cf07d75c4866bc2099c5d0e3ecba425e97e2623
Ian Heggaton <ian@pixelated.com.au>
2025-03-11 08:32:08 +1100

    Revert "Removed as not needed any more"
    This reverts commit f2aed2b8cf69aacdbca23ccf3c4e1e6a61cbc936.

---
+++ src/Factories/ProcessFactory.php
@@ -0,0 +1,29 @@
+<?php
+
+/** @noinspection PhpClassCanBeReadonlyInspection */
+
+namespace Pixelated\Streamline\Factories;
+
+use Illuminate\Support\Facades\Config;
+use Symfony\Component\Process\Process;
+
+/**
+ * This exists purely to inject a PhpProcess Object into the StreamlineUpdater class.
+ */
+class ProcessFactory
+{
+    private readonly string $processClass;
+
+    public function __construct(
+        //        #[Config('streamline.external_process_class')]
+        //        private readonly string $processClass,
+    ) {
+        // TODO restore this after upgrading to Laravel 11
+        $this->processClass = Config::get('streamline.external_process_class');
+    }
+
+    public function invoke(string $script, ?string $cwd = null, ?array $env = null, int $timeout = 60, ?array $php = null): Process
+    {
+        return new $this->processClass($script, $cwd, $env, $timeout, $php);
+    }
+}
